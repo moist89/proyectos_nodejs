fe1326e43c6b6c1c14a9031bba8d394700c072b1
moist89 <moist.medina@gmail.com>
2020-08-02 06:00:40 -0400

    añadiendo ejemplo nro 1

--- prueba_inicial_02_08_2020/router.js
+++ prueba_inicial_02_08_2020/router.js
@@ -1,10 +1,12 @@
-function route(handle, pathname) {
+function route(handle, pathname, response, postData) {
   console.log("A punto de rutear una peticion para " + pathname);
   if (typeof handle[pathname] === 'function') {
-    return handle[pathname]();
+    handle[pathname](response, postData);
   } else {
-    console.log("No se encontro manipulador para " + pathname);
-    return "404 No Encontrado";
+    console.log("No se ha encontrado manipulador para " + pathname);
+    response.writeHead(404, {"Content-Type": "text/html"});
+    response.write("404 No encontrado");
+    response.end();
   }
 }
 
--- prueba_inicial_02_08_2020/server.js
+++ prueba_inicial_02_08_2020/server.js
@@ -3,18 +3,25 @@ var url = require("url");
 
 function iniciar(route, handle) {
   function onRequest(request, response) {
-    var pathname = url.parse(request.url).pathname;
-    console.log("Peticion para " + pathname + " recibida.");
+        var dataPosteada = "";
+        var pathname = url.parse(request.url).pathname;
+        console.log("Peticion para " + pathname + " recibida.");
 
-    route(handle, pathname);
+        request.setEncoding("utf8");
 
-    response.writeHead(200, {"Content-Type": "text/html"});
-    response.write("Hola Mundo");
-    response.end();
+        request.addListener("data", function(trozoPosteado) {
+          dataPosteada += trozoPosteado;
+          console.log("Recibido trozo POST '" + trozoPosteado + "'.");
+    });
+
+    request.addListener("end", function() {
+      route(handle, pathname, response, dataPosteada);
+    });
+
   }
 
   http.createServer(onRequest).listen(8888);
-  console.log("Servidor Iniciado.");
+  console.log("Servidor Iniciado");
 }
 
 exports.iniciar = iniciar;
